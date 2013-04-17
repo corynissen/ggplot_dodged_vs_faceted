4ab4e57ce91681690cc10d169e751c4ef327cadd
corynissen <corynissen@gmail.com>
2013-04-17 12:07:38 -0500

    removed red line from dodged graph

--- ggplot_bowling.R
+++ ggplot_bowling.R
@@ -16,6 +16,5 @@ ggplot(df, aes(x=factor(name), y=score)) +
   geom_bar(position="dodge", stat="identity",aes(fill=factor(game))) +
   coord_cartesian(ylim=c(50, 150)) + scale_y_continuous(breaks=a[a%%10==0]) +
   xlab("") + ylab("Score") + scale_fill_discrete(name="Game") +
-  geom_hline(y=100, color="red", size=1) +
   opts(axis.text.x = theme_text(size=14))
 ggsave("dodged_bowling.jpg")
